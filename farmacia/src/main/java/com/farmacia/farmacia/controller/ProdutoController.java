1af72f19d1745c59b2a4e7db476d38705ce7249a
Fabiola <fabiolasou.za@live.com>
2022-02-24 15:15:30 -0300

    @fabiolasoufer [Task: CREATE endpoints]

--- farmacia/src/main/java/com/farmacia/farmacia/controller/ProdutoController.java
+++ farmacia/src/main/java/com/farmacia/farmacia/controller/ProdutoController.java
@@ -3,10 +3,15 @@ package com.farmacia.farmacia.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +41,21 @@ public class ProdutoController {
 	public ResponseEntity<List<ProdutoModel>> getByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 
+	}
+	
+	@PostMapping
+	public ResponseEntity<ProdutoModel> post (@RequestBody ProdutoModel produto){
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(repository.save(produto));
+	}
+	
+	@PutMapping
+	public ResponseEntity<ProdutoModel> put (@RequestBody ProdutoModel produto){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(produto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repository.deleteById(id);
 	}
 }
