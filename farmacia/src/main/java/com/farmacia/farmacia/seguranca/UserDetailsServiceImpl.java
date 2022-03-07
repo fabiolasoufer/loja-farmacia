1a882c554d43c7718980d1dd7c53101dde9b3515
Hikaru <hikaru.yamanaka@outlook.com>
2022-03-07 17:22:03 -0300

    @HikaruYamanaka [Task: Criando camada UserDetailsServiceĨmpl]

---
+++ farmacia/src/main/java/com/farmacia/farmacia/seguranca/UserDetailsServiceImpl.java
@@ -0,0 +1,28 @@
+package com.farmacia.farmacia.seguranca;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.farmacia.farmacia.models.Usuario;
+import com.farmacia.farmacia.repository.UsuarioRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private UsuarioRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		
+		Optional<Usuario> user = userRepository.findByUsuario(userName);
+		user.orElseThrow(() -> new UsernameNotFoundException(userName + " not found."));
+	
+		return user.map(UserDetailsImp::new).get();
+	}
+}
