116edeb4832122c9d2d2003bee8c5c03d2570128
Anita <falchi.anita@gmail.com>
2022-03-08 15:07:03 -0300

    @anita-falchi [TASK: criando usuario controller logar]

---
+++ farmacia/src/main/java/com/farmacia/farmacia/controller/UsuarioController.java
@@ -0,0 +1,41 @@
+package com.farmacia.farmacia.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.farmacia.farmacia.models.UserLogin;
+import com.farmacia.farmacia.service.UsuarioService;
+
+@RestController
+@RequestMapping("/usuarios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
+		return usuarioService.Logar(user).map(resp-> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+}
+
+	
+	
+	
+	
+	
+	
+	
